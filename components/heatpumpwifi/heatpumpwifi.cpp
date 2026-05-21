a72174d92f48376db00e890cb9b0a79ced5ff854
Nicolas Bigaouette <nbigaouette@gmail.com>
2026-05-21 14:08:27 -0400

    Debug logs

--- components/heatpumpwifi/heatpumpwifi.cpp
+++ components/heatpumpwifi/heatpumpwifi.cpp
@@ -39,17 +39,17 @@ namespace esphome
             {
                 uint8_t byte = read();
                 // Parse your protocol here
-                ESP_LOGD(TAG, "Received: 0x%02X", byte);
+                ESP_LOGV(TAG, "Received: 0x%02X", byte);
             }
         }
 
         void HeatPumpWiFi::dump_config()
         {
             ESP_LOGCONFIG(TAG, "HeatPumpWiFi:");
-            ESP_LOGE(TAG, "HeatPumpWiFi::dump_config() ERROR");
-            ESP_LOGW(TAG, "HeatPumpWiFi::dump_config() WARN");
-            ESP_LOGI(TAG, "HeatPumpWiFi::dump_config() INFO");
-            ESP_LOGD(TAG, "HeatPumpWiFi::dump_config() DEBUG");
+            ESP_LOGE(TAG, "HeatPumpWiFi::dump_config() Test ERROR");
+            ESP_LOGW(TAG, "HeatPumpWiFi::dump_config() Test WARN");
+            ESP_LOGI(TAG, "HeatPumpWiFi::dump_config() Test INFO");
+            ESP_LOGD(TAG, "HeatPumpWiFi::dump_config() Test DEBUG");
         }
 
         void HeatPumpWiFi::send_command_(uint8_t *data, size_t len)
